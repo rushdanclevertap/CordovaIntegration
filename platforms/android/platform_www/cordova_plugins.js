9a3afcf1e5942685677e0e3189c28ab1b1b0e116
rushdanbijapure-sigfyn <rushdan@clevertap.com>
2022-02-25 19:46:46 +0530

    Cordova Integration

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "clevertap-cordova.CleverTap",
+      "file": "plugins/clevertap-cordova/www/CleverTap.js",
+      "pluginId": "clevertap-cordova",
+      "clobbers": [
+        "CleverTap"
+      ]
+    },
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "clevertap-cordova": "2.4.0",
+    "cordova-android-play-services-gradle-release": "4.0.0",
+    "cordova-plugin-device": "2.0.3"
+  };
+});
